738837f2994816a1c7dd411924597e59ba43dd20
Ahmet <yevmen63@gmail.com>
2024-09-09 12:17:18 +0300

    Add project files.

---
+++ WFAEventBinding_2/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAEventBinding_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandınız");
+        }
+
+        private void label56_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kazandınız");
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandınız");
+        }
+    }
+}
